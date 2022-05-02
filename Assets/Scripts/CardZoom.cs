c89c0ae47ff45052b1271a8eb59add27dd901174
ddlan <L.danny18725@gmail.com>
2022-05-02 15:23:50 -0400

    fix cardzoom

--- Assets/Scripts/CardZoom.cs
+++ Assets/Scripts/CardZoom.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using Mirror;
 
-public class CardZoom : NetworkBehaviour
+public class CardZoom : NetworkBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     //Canvas is located at runtime during Awake(), whereas ZoomCard is a simple card prefab located in the inspector
     public GameObject Canvas;
@@ -12,17 +13,23 @@ public class CardZoom : NetworkBehaviour
 
     //zoomCard is declared here for later usage, while zoomSprite is assigned during Awake() to store this gameobject's sprite
     private GameObject zoomCard;
-    private Sprite zoomSprite;
+
+    float x1;
+    float x2;
+    float y1;
+    float y2;
 
     public void Awake()
     {
         Canvas = GameObject.Find("Main Canvas");
-        zoomSprite = gameObject.GetComponent<Image>().sprite;
     }
 
     //OnHoverEnter() is called by the Pointer Enter event in the Event Trigger component attached to this gameobject
-    public void OnHoverEnter()
+    public void OnPointerEnter(PointerEventData pointerEventData)
     {
+        // only create one zoom card at a time
+        if (zoomCard != null) return;
+
         //determine whether the client hasAuthority over this gameobject
         bool cardBeenPlayed = gameObject.GetComponent<DragDrop>().beenPlayed;
         Debug.Log("onHoverEnter" + cardBeenPlayed);
@@ -30,25 +37,33 @@ public class CardZoom : NetworkBehaviour
 
         //if the client hasAuthority, create a new version of the card with the appropriate sprite
     
-        int width = 240;
-        int height = 344;
+        int width = 120;
+        int height = 177;
         int x = (Screen.width / 2) + (width / 2);
         int y = (Screen.height / 2) + (height / 2);
 
         zoomCard = Instantiate(ZoomCard, new Vector2(x,y), Quaternion.identity);
-        zoomCard.GetComponent<Image>().sprite = zoomSprite;
         
         //make the card a child of the Canvas so that it is rendered on top of everything else
         zoomCard.transform.SetParent(Canvas.transform, true);
-
-        //make the card bigger!
-        RectTransform rect = zoomCard.GetComponent<RectTransform>();
-        rect.sizeDelta = new Vector2(width, height);
     }
 
     //OnHoverExit() is called by the Pointer Exit event (as well as Begin Drag) in the Event Trigger component attached to this gameobject
-    public void OnHoverExit()
+    public void OnPointerExit(PointerEventData pointerEventData)
     {
+        // Read card dimensions at runtime after instantiation
+        if (x1 == 0.0f) {
+            x1 = gameObject.transform.position.x - (GetComponent<BoxCollider2D>().size.x / 2);
+            x2 = gameObject.transform.position.x + (GetComponent<BoxCollider2D>().size.x / 2);
+            y1 = gameObject.transform.position.y - (GetComponent<BoxCollider2D>().size.y / 2);
+            y2 = gameObject.transform.position.y + (GetComponent<BoxCollider2D>().size.y / 2);
+        }
+
+        // Don't destroy card if still within card bounds (aka hovering over child of card object)
+        if (pointerEventData.position.x > x1 && pointerEventData.position.x < x2 &&
+            pointerEventData.position.y > y1 && pointerEventData.position.y < y2) return;
+
         Destroy(zoomCard);
+        zoomCard = null;
     }
 }
