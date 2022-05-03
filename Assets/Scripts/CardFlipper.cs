042769a839627dda7d6af8c318762117cb713e96
ddlan <L.danny18725@gmail.com>
2022-05-02 23:17:13 -0400

    fix card back for enemy hand

--- Assets/Scripts/CardFlipper.cs
+++ Assets/Scripts/CardFlipper.cs
@@ -5,23 +5,9 @@ using UnityEngine.UI;
 
 public class CardFlipper : MonoBehaviour
 {
-    //store the card front and back sprites, located in the inspector
-    public Sprite CardFront;
-    public Sprite CardBack;
-
     public void Flip()
     {
-        //when Flip() is called, store the value of the current sprite attached to this gameobject
-        Sprite currentSprite = gameObject.GetComponent<Image>().sprite;
-
-        // Conditional logic to determine whether to display the card front or back sprite
-        if (currentSprite == CardFront)
-        {
-            gameObject.GetComponent<Image>().sprite = CardBack;
-        }
-        else
-        {
-            gameObject.GetComponent<Image>().sprite = CardFront;
-        }
+        GameObject cardBack = gameObject.transform.Find("CardBack").gameObject;
+        cardBack.SetActive(!cardBack.activeSelf);
     }
 }
